5d6b206be4b01e14ce9e325f3471ae8ddbe70c2f
alazarte <alazarte@unlam.edu.ar>
2019-08-15 15:58:20 -0300

    Rediseño de la pantalla créditos con íconos y nueva animación

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.Audio;
 public class SettingsMenu : MonoBehaviour {
 
 	public AudioMixer audioMixer;
+	public GameObject icono;
 
 	public void SetVolume(float volume){
 		audioMixer.SetFloat("volume",volume);
