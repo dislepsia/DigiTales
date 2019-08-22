abf2738904195365dfe1de13033376b4866a27bd
alazarte <alazarte@unlam.edu.ar>
2019-08-22 16:04:38 -0300

    Incorporación de script para el efecto Parallax del fondo Retorno al listado de cuentos luego de "cargar" el codigo

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+	float scrollSpeed = -10f;
+	Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPos=0f;
+
+
+			newPos = Mathf.Repeat(Time.time * scrollSpeed, 200);
+
+		transform.position = startPos + Vector2.right * newPos;
+		Debug.Log ("Verdadero: " +transform.position);
+	}
+}
