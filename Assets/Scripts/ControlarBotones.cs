00356e4fa3bff7431e1bcc128477b37274e8231b
alazarte <alazarte@unlam.edu.ar>
2019-08-26 17:35:55 -0300

    Se incorporo la pantalla de selección de modo Se añadio la pantalla del MiniJuego Se agrandaron los iconos del menú

--- Assets/Scripts/ControlarBotones.cs
+++ Assets/Scripts/ControlarBotones.cs
@@ -12,11 +12,13 @@ public class ControlarBotones : MonoBehaviour
 
     public void MenuPrincipal()
     {
+		Screen.orientation = ScreenOrientation.Portrait;
         SceneManager.LoadScene("NewMenu");
     }
 
     public void Creditos()
     {
+		Screen.orientation = ScreenOrientation.Portrait;
         SceneManager.LoadScene("NewCreditos");
     }
 
@@ -25,23 +27,38 @@ public class ControlarBotones : MonoBehaviour
         SceneManager.LoadScene("Cuento1Escena1");        
     }
 
+	public void ElegirModo()
+	{
+		Screen.orientation = ScreenOrientation.Portrait;
+		SceneManager.LoadScene("MiniJuegoCuento");        
+	}
+
+	public void MiniJuego()
+	{
+		SceneManager.LoadScene("Juego");        
+	}
+
     public void ListadoCuentos()
     {		
+		Screen.orientation = ScreenOrientation.Portrait;
         SceneManager.LoadScene("NewListadoCuentos");
     }
 		
 	public void Opciones()
 	{
+		Screen.orientation = ScreenOrientation.Portrait;
 		SceneManager.LoadScene("Opciones");
 	}
 
 	public void Descargar()
 	{
+		Screen.orientation = ScreenOrientation.Portrait;
 		SceneManager.LoadScene("CargaDeCodigo");
 	}
 
 	public void Salir()
 	{
+		Screen.orientation = ScreenOrientation.Portrait;
 		SceneManager.LoadScene("Salir");
 	}
 }
