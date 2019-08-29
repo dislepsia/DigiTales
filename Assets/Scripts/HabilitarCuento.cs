b8b669dbf71f40e6daf57d09ba1874ef3a6f3508
alazarte <alazarte@unlam.edu.ar>
2019-08-29 17:36:29 -0300

    Se incorporo la habilitacion de cuentos a traves de la pantalla de carga. Se utiliza el codigo 6281 para los chanchitos y 5555 para caperucita.

---
+++ Assets/Scripts/HabilitarCuento.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HabilitarCuento : MonoBehaviour {
+
+	public string cuento = "";
+	public GameObject objectToDisable;
+
+	public static HabilitarCuento objetoHabilitar= new HabilitarCuento();
+	public string codigo = null;
+
+	//Habilitar todos los botones que fueron desbloqueados anteriormente (Teniendo ya el archivo)
+	void Start () {
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		//foreach(string desbloquear in codigo)
+		//{
+		
+			if(HabilitarCuento.objetoHabilitar.codigo !=null){
+
+				cuento = HabilitarCuento.objetoHabilitar.codigo;
+
+				if (cuento.Equals("6281")){
+					GameObject.Find ("ChanchitosButton").GetComponent<Button> ().interactable = true;
+				}
+
+				if (cuento.Equals ("5555")) {
+					GameObject.Find ("CaperucitaButton").GetComponent<Button> ().interactable = true;
+				} 
+			}
+
+		}
+	//}
+}
---
+++ Assets/Scripts/ScriptCodigoCuento.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScriptCodigoCuento : MonoBehaviour {
+
+	public void Update(){
+		//Transferencia del código ingresado a la escena del listado de cuentos
+		HabilitarCuento.objetoHabilitar.codigo = GameObject.Find ("TextMeshPro - InputField").GetComponent<TMP_InputField> ().text;
+	}
+}
