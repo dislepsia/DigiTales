03b2ccd214b71c613d39c627d287241e2367d51a
alazarte <alazarte@unlam.edu.ar>
2019-08-30 17:18:09 -0300

    Se comenzo la lógica para realizar la grabación de los codigos que se van cargandos

---
+++ Assets/Scripts/CargarCodigosDesbloqueados.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CargarCodigosDesbloqueados : MonoBehaviour {
+
+	public TMP_InputField entrada;
+	public Text salida;
+	string codigosAnteriores = "";
+
+	public void Guardar () {
+		PlayerPrefs.SetString ("CodigoCuento", entrada.text);
+	}
+	
+	// Update is called once per frame
+	void Start () {
+		salida.text = PlayerPrefs.GetString ("CodigoCuento");
+
+	}
+		
+}
---
+++ Assets/Scripts/CodigosGuardados.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class CodigosGuardados : MonoBehaviour {
+
+	public static string codigosHabilitados = "";
+	private string rutaArchivo;
+
+	void Awake () {
+		Debug.Log (Application.persistentDataPath);
+		rutaArchivo = Application.persistentDataPath + "/datos.dat";
+	}
+
+	void Start () {
+		Cargar ();
+	}
+
+	public void Guardar () {
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (rutaArchivo);
+
+		GuardarDatos codigos = new GuardarDatos ();
+		codigos.habilitados = codigos.ToString() + "-";
+
+		bf.Serialize (file, codigos);
+		file.Close();
+		Debug.Log ("CodigosHabilitados: "+codigosHabilitados);
+		Debug.Log ("Guardar:" +codigosHabilitados);
+	}
+
+	// Update is called once per frame
+	void Cargar () {
+		if(File.Exists(rutaArchivo)){
+
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Open (rutaArchivo, FileMode.Open);
+
+		GuardarDatos codigos = (GuardarDatos)bf.Deserialize (file);
+		codigosHabilitados= codigos.habilitados;
+		file.Close();
+		Debug.Log ("Cargar:"+codigosHabilitados);
+		}
+	}
+}
+
+[Serializable]
+class GuardarDatos{
+
+	public string habilitados;
+
+}
