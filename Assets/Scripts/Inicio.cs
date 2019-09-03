2dc37be6690a29867adbab25530904a7c86baf47
maty222528 <mepatrig@gmail.com>
2019-09-03 18:46:31 -0300

    Agregada pantalla de Inicio con Logo

---
+++ Assets/Scripts/Inicio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inicio : MonoBehaviour {
+
+
+	void Start()
+	{
+		StartCoroutine (SpriteShapeOut());
+		StopCoroutine ("SpriteShapeOut");
+	}  
+
+	IEnumerator SpriteShapeOut()
+	{	
+		yield return new WaitForSeconds(3f);	
+		SceneManager.LoadScene("NewMenu");
+	}
+}
