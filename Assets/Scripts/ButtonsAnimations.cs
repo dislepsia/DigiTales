8499461d13a3c562b713d80a6b43ca5df2be3b6b
alazarte <alazarte@unlam.edu.ar>
2019-09-10 14:39:41 -0300

    Se incluyo archivos (2 scripts+ icono) que no fueron subidos en el anterior commit

---
+++ Assets/Scripts/ButtonsAnimations.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsAnimations : MonoBehaviour {
+
+	[Range(0.25f,5f)]
+	public float scaleSpeed = 0.4f;
+	public AnimationCurve aCurve;
+	private Transform _transform;
+	private float step;
+	private float objScale;
+
+	// Use this for initialization
+	public void Start () {
+		_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		step += scaleSpeed * Time.deltaTime;
+		objScale = aCurve.Evaluate (step);
+		_transform.localScale = new Vector2 (objScale, objScale);
+		if (step >= 1) {
+			step = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/ResetearPlayerPrefabs.cs
@@ -0,0 +1,14 @@
+﻿//using UnityEngine;
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+public class ResetearPlayerPrefabs : EditorWindow
+{
+	[MenuItem("Window/Delete PlayerPrefs (All)")]
+	static void DeleteAllPlayerPrefs()
+	{
+		UnityEngine.PlayerPrefs.DeleteAll();
+	}
+}
+#endif
